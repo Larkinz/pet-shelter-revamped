0b758a381610ff9836fdce25a95e5358b85caa29
Larkinz <andy_larkin33@hotmail.com>
2024-06-18 01:49:25 +0200

    feat&style: added mobile navigation button menu and functionality

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+// mobile navigation functionality
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
